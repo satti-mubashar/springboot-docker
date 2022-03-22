a3d61a665ea9981a78b9ad35cc42abad87993d29
SYSTEMS\muhammad.mubasharali <satti.mubashar@hotmail.com>
2022-03-22 15:57:43 +0500

    First commit

---
+++ src/main/java/com/javatechie/k8s/SpringbootK8sDemoApplication.java
@@ -0,0 +1,22 @@
+package com.javatechie.k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootK8sDemoApplication {
+
+	@GetMapping("/message")
+	public String displayMessage(){
+		System.out.println("Congratulation you successfully deployed your application to kubernetes !!");
+		return "Congratulation you successfully deployed your application to kubernetes !!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootK8sDemoApplication.class, args);
+	}
+
+}
